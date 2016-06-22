3c72d525406921b02a7b0b58576d9b17c52fd302
j <jamie.williams89@gmail.com>
2016-06-22 09:13:44 +0000

    Moved index.clj tests to seperate file

--- test/clj_git/core_test.clj
+++ test/clj_git/core_test.clj
@@ -3,7 +3,6 @@
             [clj-git.util :refer :all]
             [clj-git.repo :refer :all]
             [clj-git.object :refer :all]
-            [clj-git.index :refer :all]
             [clj-git.core :refer :all]))
 
 (deftest t-hash-str
@@ -57,48 +56,6 @@
                :hash "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391"}])))
   )
 
-(deftest t-read-index
-  (testing "Reading 'first_index' fixture into a struct")
-    (with-redefs [git-root (fn [] "test/fixture1/")]
-      (is (= (read-index)
-             [ {:inode 2099386, :device 16777218, :filesize 0x5,
-                :hash "9daeafb9864cf43055ae93beb0afd6c7d144bfa4"
-                :name "test_file"
-                :ctime #inst "2016-01-22T19:21:34.000-00:00"
-                :mtime #inst "2016-01-22T19:21:34.000-00:00"}])))
-    )
-
-(deftest t-file-tree
-  (testing "Build file tree structure from example file list")
-    (is (= (files-tree [{:name "test_file" :hash "A"}
-                        {:name "test/clj_git/core_test.clj" :hash "B"}
-                        {:name "test/clj_git/test" :hash "C"}])
-           {"test_file"
-              {:type :file
-               :parent nil
-               :hash "A"
-               :children []}
-            "test/clj_git/test"
-              {:type :file
-               :parent "test/clj_git"
-               :hash "C"
-               :children []}
-            "test"
-              {:type :dir
-               :parent nil
-               :children ["test/clj_git"]}
-            "test/clj_git"
-              {:type :dir
-               :parent "test"
-               :children ["test/clj_git/core_test.clj"
-                          "test/clj_git/test"]}
-            "test/clj_git/core_test.clj"
-              {:type :file
-               :parent "test/clj_git"
-               :hash "B"
-               :children []}}))
-)
-
 (deftest t-hash-tree
   (testing "Check that trees hash to original values")
     (is (= (hash-tree (tree "6811239b02050711578e223ffe1c297a0eb801f4"))
@@ -115,17 +72,3 @@
            "ca93b49848670d03b3968c8a481eca55f5fb2150"))
 )
 
-(deftest t-index-to-tree-objects
-  (testing "fixture1 index hash the correct top level hash")
-  (with-redefs [git-root (fn [] "test/fixture1/")]
-    (is (= (first (index-to-tree-objects))
-           "ca93b49848670d03b3968c8a481eca55f5fb2150")))
-  (testing "fixture2 index hash the correct top level hash")
-  (with-redefs [git-root (fn [] "test/fixture2/")]
-    (is (= (first (index-to-tree-objects))
-           "40951cd74421289606500f35d0c6ccb50b552b4b")))
-  (testing "fixture3 index hash the correct top level hash")
-  (with-redefs [git-root (fn [] "test/fixture3/")]
-    (is (= (first (index-to-tree-objects))
-           "4b90f754d0ddf46643fb5250b47cddfb43102f30")))
-)
---
+++ test/clj_git/index_test.clj
@@ -0,0 +1,61 @@
+(ns clj-git.index-test
+  (:require [clojure.test :refer :all]
+            [clj-git.repo :refer :all]
+            [clj-git.index :refer :all]))
+
+(deftest t-read-index
+  (testing "Reading 'first_index' fixture into a struct")
+    (with-redefs [git-root (fn [] "test/fixture1/")]
+      (is (= (read-index)
+             [ {:inode 2099386, :device 16777218, :filesize 0x5,
+                :hash "9daeafb9864cf43055ae93beb0afd6c7d144bfa4"
+                :name "test_file"
+                :ctime #inst "2016-01-22T19:21:34.000-00:00"
+                :mtime #inst "2016-01-22T19:21:34.000-00:00"}])))
+    )
+
+(deftest t-file-tree
+  (testing "Build file tree structure from example file list")
+    (is (= (files-tree [{:name "test_file" :hash "A"}
+                        {:name "test/clj_git/core_test.clj" :hash "B"}
+                        {:name "test/clj_git/test" :hash "C"}])
+           {"test_file"
+              {:type :file
+               :parent nil
+               :hash "A"
+               :children []}
+            "test/clj_git/test"
+              {:type :file
+               :parent "test/clj_git"
+               :hash "C"
+               :children []}
+            "test"
+              {:type :dir
+               :parent nil
+               :children ["test/clj_git"]}
+            "test/clj_git"
+              {:type :dir
+               :parent "test"
+               :children ["test/clj_git/core_test.clj"
+                          "test/clj_git/test"]}
+            "test/clj_git/core_test.clj"
+              {:type :file
+               :parent "test/clj_git"
+               :hash "B"
+               :children []}}))
+)
+
+(deftest t-index-to-tree-objects
+  (testing "fixture1 index hash the correct top level hash")
+  (with-redefs [git-root (fn [] "test/fixture1/")]
+    (is (= (first (index-to-tree-objects))
+           "ca93b49848670d03b3968c8a481eca55f5fb2150")))
+  (testing "fixture2 index hash the correct top level hash")
+  (with-redefs [git-root (fn [] "test/fixture2/")]
+    (is (= (first (index-to-tree-objects))
+           "40951cd74421289606500f35d0c6ccb50b552b4b")))
+  (testing "fixture3 index hash the correct top level hash")
+  (with-redefs [git-root (fn [] "test/fixture3/")]
+    (is (= (first (index-to-tree-objects))
+           "4b90f754d0ddf46643fb5250b47cddfb43102f30")))
+)
