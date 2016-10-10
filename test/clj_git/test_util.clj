4a6f2464a9199f789e8a52fb3c5fc9a81451c390
j <jamie.williams89@gmail.com>
2016-10-10 15:56:26 +0000

    with-repo-sandbox checks out a clean working copy

--- test/clj_git/test_util.clj
+++ test/clj_git/test_util.clj
@@ -1,5 +1,6 @@
 (ns clj-git.test-util
   (:require [clojure.test :refer :all]
+            [clojure.java.shell :refer :all]
             [clj-git.repo :refer :all]
             [clj-git.util :refer :all])
   (:gen-class))
@@ -9,11 +10,13 @@
       (list 'assert (list '.isDirectory (list 'clojure.java.io/as-file bare-repo-path)))
       (list 'assert (list 'not (list '.exists (list 'clojure.java.io/as-file sandbox-pathname))))
       (list 'assert (list '.mkdir (list 'clojure.java.io/as-file sandbox-pathname)))
-      (list 'ok-sh "cp" "-rf" bare-repo-path (str sandbox-pathname "/.git"))
-      (list 'with-redefs ['git-root (list 'fn [] (str sandbox-pathname "/.git"))]
-        (list 'let ['res (cons 'do body)]
-          (list 'ok-sh "rm" "-rf" sandbox-pathname)
-          'res))))
+      (list 'ok-sh "cp" "-rf" bare-repo-path (str sandbox-pathname "/.git/"))
+      (list 'with-redefs ['repo-root (list 'fn [] (str sandbox-pathname "/"))]
+        (list 'with-sh-dir (list 'repo-root)
+          (list 'ok-sh "git" "checkout" ".")
+          (list 'let ['res (cons 'do body)]
+            (list 'ok-sh "rm" "-rf" (str "../" sandbox-pathname))
+            'res)))))
 
 (deftest t-with-repo-sandbox
   (with-repo-sandbox "fixtures/base_repo.git" "t-with-repo-sandbox"
@@ -22,7 +25,15 @@
       (is (.exists (clojure.java.io/as-file "t-with-repo-sandbox/.git/index")))
     )
     (testing "(git-root) points to the correct location"
-      (is (= (git-root) "t-with-repo-sandbox/.git"))
+      (is (= (git-root) "t-with-repo-sandbox/.git/"))
+    )
+  )
+)
+
+(deftest t-bare-repo-sandbox
+  (with-repo-sandbox "fixtures/base_repo.git" "t-bare-repo-sandbox"
+    (testing "test_file correctly checked out"
+      (is (.exists (clojure.java.io/as-file (str (repo-root) "test_file"))))
     )
   )
 )
