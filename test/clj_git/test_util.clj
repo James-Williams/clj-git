84f8d62e7676b7860de9e7950396fced0a1383d4
j <jamie.williams89@gmail.com>
2016-10-15 21:04:43 +0000

    with-repo-sandbox: throw the original exception after cleanup

--- test/clj_git/test_util.clj
+++ test/clj_git/test_util.clj
@@ -11,13 +11,16 @@
       (list 'assert (list '.mkdir (list 'clojure.java.io/as-file sandbox-pathname)))
       (list 'ok-sh "cp" "-rf" bare-repo-path (str sandbox-pathname "/.git/"))
       (list 'with-redefs ['clj-git.repo/repo-root (list 'fn [] (str sandbox-pathname "/"))]
-        (list 'clojure.java.shell/with-sh-dir (list 'clj-git.repo/repo-root)
-          (cons 'do (concat
-            (list (list 'ok-sh "git" "checkout" "."))
-            (list (list 'try
-              (cons 'do body)
-              (list 'catch 'Exception 'e)
-              (list 'finally (list 'ok-sh "rm" "-rf" (str "../" sandbox-pathname)))))))))))
+        (list 'let (vector 'err '(atom nil))
+          (list 'clojure.java.shell/with-sh-dir (list 'clj-git.repo/repo-root)
+            (cons 'do (concat
+              (list (list 'ok-sh "git" "checkout" "."))
+              (list (list 'try
+                (cons 'do body)
+                (list 'catch 'Exception 'e '(reset! err e))
+                (list 'finally
+                  (list 'ok-sh "rm" "-rf" (str "../" sandbox-pathname))
+                  '(if @err (throw @err))))))))))))
 
 (deftest t-with-repo-sandbox
   (with-repo-sandbox "fixtures/base_repo.git" "t-with-repo-sandbox"
@@ -33,9 +36,9 @@
 
 (deftest t-with-repo-sandbox-cleanup
   (testing "with-repo-sandbox cleanups up dir after exception"
-    (is (thrown? Exception
+    (is (thrown-with-msg? Exception #"throw from test"
       (with-repo-sandbox "fixtures/base_repo.git" "t-with-repo-sandbox-cleanup"
-        (throw (Exception.)))))
+        (throw (Exception. "throw from test")))))
     (is (not (.exists (clojure.java.io/as-file "t-with-repo-sandbox-cleanup"))))
   )
 )
